463a815860f461b6141bbda9260dd166aa008081
RAKIB <rakibmahmudmridha@gmail.com>
2023-11-06 16:46:58 +0600

    files modified

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,7 +1,34 @@
 let userDb = require("../model/model");
 
 // create and save new user
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  // validate request
+  if (!req.body) {
+    res.status(400).json({
+      message: "Content can not empty",
+    });
+  }
+
+  // new user
+  const user = new UserDb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+  });
+
+  // save user in the database
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message:
+          err.message || "Some error occured while creating a create operation",
+      });
+    });
+};
 
 // retrieve and return all users
 exports.find = (req, res) => {};
